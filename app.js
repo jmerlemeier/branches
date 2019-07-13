d837bcb0b3925c793b23ed6793b57d0a005900bd
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-12 17:32:27 -0700

    Completed Q4 and passed test.

--- app.js
+++ app.js
@@ -90,14 +90,32 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
+var i = 0
 
 function sumArray(sumArr) { //eslint-disable-line
+  var totalContents = 0
 
+  for(var i = 0; i < sumArr.length; i++){
+      totalContents = parseInt(sum(sumArr[i], totalContents)[0]);
+  }
+console.log(totalContents);
+
+
+  var stringOutput = sumArr + ' was passed in as an array of numbers, and ' + totalContents + ' is their sum.';
+  
+  
+  
+  console.log(stringOutput);
+  
+  return [totalContents, stringOutput];
 }
+
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -112,8 +130,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+var multipleArray = [2, 3, 4];
 
+function multiplyArray(multArr) { //eslint-disable-line
+  var totalContents = 0
+
+  for(var i = 0; i < multArr.length; i++){
+      totalContents = sum(multArr[i], totalContents)[0];
+  }
+console.log(totalContents);
+
+
+  var stringOutput = 'The numbers ' + multArr + ' have a product of ' + totalContents;
+  
+  
+  
+  console.log(stringOutput);
+  
+  return [totalContents, stringOutput];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
